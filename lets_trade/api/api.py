c4d83db63ec6a1345bf8ffe340d71b30a0ef8807
AntonNguyen <afnguyen85@gmail.com>
2012-09-14 20:07:16 -0400

    Added serializer for sexy json

--- lets_trade/api/api.py
+++ lets_trade/api/api.py
@@ -1,7 +1,10 @@
 from tastypie.resources import ModelResource
 from django.contrib.auth.models import User
+from resource import PrettyJSONSerializer
 
 class UserResource(ModelResource):
 	class Meta:
 		queryset = User.objects.all()
 		resource_name = 'user'
+		fields = ['username', 'first_name', 'last_name', 'last_login']
+		serializer = PrettyJSONSerializer()
