9ef1a232f416b6f638919f0b8ca10e0664ad0070
RaeDar <joear0701@gmail.com>
2012-09-17 20:34:21 -0400

    item model

--- lets_trade/api/models.py
+++ lets_trade/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=80)
+    city = models.CharField(max_length=30)
+    postal_code = models.CharField(max_length=30)
+    description = models.TextField()
